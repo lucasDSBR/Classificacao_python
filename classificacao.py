320c3062ce1776823edae8b37eaa7ef3523bb9f2
AkinoriKoerich <yuukko404@gmail.com>
2023-04-09 07:13:03 -0300

    Atualização

--- classificacao.py
+++ classificacao.py
@@ -1,43 +1,50 @@
-from sklearn.datasets import load_iris, fetch_kddcup99
+from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, export_text, plot_tree
 from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 
-################## Pré-processamento ###################
-# Coleta e Integração
+################## Prï¿½-processamento ###################
+# Coleta e Integraï¿½ï¿½o
 iris = load_iris()
 
 caracteristicas = iris.data
 rotulos = iris.target
 
 print("Caracteristicas:\n", caracteristicas[:2])
-print("Rótulos:\n", rotulos[:2])
+print("Rï¿½tulos:\n", rotulos[:2])
 print('########################################################')
 
-# Partição dos dados
+# Partiï¿½ï¿½o dos dados
 carac_treino, carac_teste, rot_treino, rot_teste = train_test_split(caracteristicas, rotulos)
 
-################### Mineração #####################
+################### Mineraï¿½ï¿½o #####################
 
-############---------- Arvore de Decisão -----------############
+############---------- Arvore de DecisÃ£o -----------############
 arvore = DecisionTreeClassifier(max_depth=2)
 arvore.fit(X=carac_treino, y=rot_treino)
 
 rot_preditos = arvore.predict(carac_teste)
 acuracia_arvore = accuracy_score(rot_teste, rot_preditos)
-############-------- Máquina de Vetor Suporte ------############
+
+# adicionando visualizaÃ§Ã£o da Ã¡rvore
+fig, ax = plt.subplots(figsize=(12, 12))
+plot_tree(arvore, filled=True, feature_names=iris.feature_names, class_names=iris.target_names, ax=ax)
+plt.show()
+
+############-------- Mï¿½quina de Vetor Suporte ------############
 clf = SVC()
 clf.fit(X=carac_treino, y=rot_treino)
 
 rot_preditos_svm = clf.predict(carac_teste)
 acuracia_svm = accuracy_score(rot_teste, rot_preditos_svm)
 
-################ Pós-processamento ####################
-print("Acurácia Árvore de Decisão:", round(acuracia_arvore, 5))
-print("Acurácia SVM:", round(acuracia_svm, 5))
+################ Pï¿½s-processamento ####################
+print("Acurï¿½cia ï¿½rvore de Decisï¿½o:", round(acuracia_arvore, 5))
+print("Acurï¿½cia SVM:", round(acuracia_svm, 5))
 print('########################################################')
 
 r = export_text(arvore, feature_names=iris['feature_names'])
-print("Estrutura da árvore")
+print("Estrutura da ï¿½rvore")
 print(r)
